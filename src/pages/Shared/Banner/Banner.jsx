94ce9fa469c9a4830aa752cbad9f25514cfeb3b4
mirazfaruk <mirazfaruk@gmail.com>
2024-08-28 16:19:00 +0600

    Added slider

--- src/pages/Shared/Banner/Banner.jsx
+++ src/pages/Shared/Banner/Banner.jsx
@@ -1,31 +1,42 @@
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
 import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import img1 from '../../../assets/images/balcony-with-view-city-planter-with-view-city-background.jpg';
-import img2 from '../../../assets/images/luxury-pool-villa-spectacular-contemporary-design-digital-art-real-estate-home-house-property-ge.jpg';
-import img3 from '../../../assets/images/umbrella-chair-around-swimming-pool.jpg';
+import 'swiper/css/navigation'; // Import Swiper styles
 
 const Banner = () => {
     return (
-        <Swiper className='max-h-screen mt-20'
-            // install Swiper modules
-            modules={[Navigation, Pagination, Scrollbar, A11y]}
-            // spaceBetween={50}
-            // slidesPerView={3}
-            navigation
-            pagination={{ clickable: true }}
-            scrollbar={{ draggable: true }}
-            onSwiper={(swiper) => console.log(swiper)}
-            onSlideChange={() => console.log('slide change')}
-        >
-            <SwiperSlide><img className='w-full' src={img1} alt="" /></SwiperSlide>
-            <SwiperSlide><img className='w-full' src={img1} alt="" /></SwiperSlide>
-            <SwiperSlide><img className='w-full' src={img1} alt="" /></SwiperSlide>
-            <SwiperSlide><img className='w-full' src={img1} alt="" /></SwiperSlide>
-        </Swiper>
+        <div>
+            <Swiper
+                modules={[Navigation]} // Corrected to use Navigation
+                spaceBetween={50}
+                slidesPerView={1}
+                navigation
+                loop={true}
+            >
+                {/* Swiper Slides */}
+                <SwiperSlide>
+                    <img 
+                        src={`https://i.ibb.co/kh9FrD0/daniel-barnes-RKd-Ll-Tyjm5g-unsplash.jpg`} 
+                        alt="Slide 1"
+                        className="w-full max-h-screen"
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img 
+                        src={`https://i.ibb.co/m8t0bFz/luxury-pool-villa-spectacular-contemporary-design-digital-art-real-estate-home-house-property-ge.jpg`}
+                        alt="Slide 2"
+                        className="w-full max-h-screen"
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img 
+                        src={`https://i.ibb.co/LPGSjyk/umbrella-chair-around-swimming-pool.jpg`} 
+                        alt="Slide 3"
+                        className="w-full max-h-screen"
+                    />
+                </SwiperSlide>
+            </Swiper>
+        </div>
     );
 };
 
